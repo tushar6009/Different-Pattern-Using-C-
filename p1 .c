41f61684b35a1286a7060f9eaa2033a0570cd74e
Tushar Jogani <163539198+tushar6009@users.noreply.github.com>
2024-03-23 16:41:38 +0530

    Add files via upload

---
+++ p1 .c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+main(){
+	
+	
+	printf("\n\n");
+	printf("\t*\t\t\t*\t*\t*\t*\n\n");
+	printf("\t*\t\t\t*\n\n");
+	printf("\t*\t\t\t*\n\n");
+	printf("\t*\t\t\t*\n\n");
+	printf("\t*\t\t\t*\n\n");
+	printf("\t*\t*\t*\t*\t*\t*\t*\n\n");
+	printf("\t\t\t\t*\t\t\t*\n\n");
+	printf("\t\t\t\t*\t\t\t*\n\n");
+	printf("\t\t\t\t*\t\t\t*\n\n");
+	printf("\t\t\t\t*\t\t\t*\n\n");
+	printf("\t*\t*\t*\t*\t\t\t*\n\n");
+	
+	printf("\n\n\n\n\n\n");
+	
+	printf("\t\t\t\t\t*\n\n\n");
+	printf("\t\t\t\t*\t*\t*\n\n\n");
+	printf("\t\t\t*\t*\t*\t*\t*\n\n\n");
+	printf("\t\t*\t*\t*\t*\t*\t*\t*\n\n\n");
+	printf("\t*\t*\t*\t*\t*\t*\t*\t*\t*\n\n\n");
+	
+	
+	printf("\n\n\n\n\n\n");
+	
+	
+	printf("\t*\t\t\t\t\t\t\t\t\t*\n\n");
+	printf("\t\t*\t\t\t\t\t\t\t*\n\n");
+	printf("\t\t\t*\t\t\t\t\t*\n\n");
+	printf("\t\t\t\t*\t\t\t*\n\n");
+	printf("\t\t\t\t\t*\t*\n\n");
+	printf("\t\t\t\t\t    *\n\n");
+	printf("\t\t\t\t\t*\t*\n\n");
+	printf("\t\t\t\t*\t\t\t*\n\n");
+	printf("\t\t\t*\t\t\t\t\t*\n\n");
+	printf("\t\t*\t\t\t\t\t\t\t*\n\n");
+	printf("\t*\t\t\t\t\t\t\t\t\t*\n\n");
+	
+	
+	printf("\n\n\n\n\n\n");
+	
+	
+	printf("\t\t*\t*\t*\t*\t*\n\n");
+	printf("\t\t\t*\t*\t*\t*\n\n");
+	printf("\t\t\t\t*\t*\t*\n\n");
+	printf("\t\t\t\t\t*\t*\n\n");
+	printf("\t\t\t\t\t\t*\n\n");
+	printf("\t\t\t\t\t*\t*\n\n");
+	printf("\t\t\t\t*\t*\t*\n\n");
+	printf("\t\t\t*\t*\t*\t*\n\n");
+	printf("\t\t*\t*\t*\t*\t*\n\n");
+	
+	
+}
